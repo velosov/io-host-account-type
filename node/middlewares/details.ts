9f1e90a33d0f99ed80ee9fa67739fb403debe820
velosov <viniciusveloso@poli.ufrj.br>
2024-08-16 17:20:05 -0300

    Adds correct policy for further details

--- node/middlewares/details.ts
+++ node/middlewares/details.ts
@@ -2,8 +2,9 @@ export async function details(ctx: Context) {
     const { clients: { licenseManager } , vtex: { authToken, logger } } = ctx
 
     try {
-        const attempt = await licenseManager.getTopbarData(authToken) //https://github.com/vtex/node-vtex-api/blob/bfe0cae9247be60df7261dd37639e2e5a61deee1/src/clients/janus/LicenseManager.ts#L37 ; getAccountData @ #L21 gives 403
-        ctx.body = attempt
+        const details = await licenseManager.getAccountData(authToken) //https://github.com/vtex/node-vtex-api/blob/bfe0cae9247be60df7261dd37639e2e5a61deee1/src/clients/janus/LicenseManager.ts#L21
+        // const attempt = await licenseManager.getTopbarData(authToken) //https://github.com/vtex/node-vtex-api/blob/bfe0cae9247be60df7261dd37639e2e5a61deee1/src/clients/janus/LicenseManager.ts#L37
+        ctx.body = details
     } catch(e) { logger.error(e); throw new Error(`${e}`) }
     return
 }
